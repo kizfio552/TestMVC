2c24227318ba77bc8a54690785f88b920a7a896b
kizfio552 <kizfio552@gmail.com>
2023-06-10 22:03:44 +0700

    Test MVC

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        public IActionResult Welcome(string name, int numTimes = 5)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+        }
+
+        public string Welcome2(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
